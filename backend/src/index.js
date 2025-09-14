7002bd65a868e31b122715a5b7f8daa23c03aaa2
rahulgope45 <rahuleile456@gmail.com>
2025-09-14 22:33:02 +0530

    auth conroller and path added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import authRoutes from "./routes/auth.route.js"
 
 const app = express();
+
+//Making auth
+app.use("/api/auth", authRoutes )
 
 app.listen(5001, () => {
     console.log("Server is running on port 5001")
