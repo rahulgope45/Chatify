e212cc93519f7cbad51852214e3fd6b12b6d01e4
rahulgope45 <rahuleile456@gmail.com>
2025-09-18 22:04:44 +0530

    Connected cluster to the app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,21 @@
 import express from 'express'
 import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+
+import { connectDB } from './lib/db.js';
+
 
 const app = express();
+dotenv.config()
+
+const PORT = process.env.PORT
+
 
 //Making auth
 app.use("/api/auth", authRoutes )
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001")
+app.listen(PORT, () => {
+    console.log("Server is running on port "+ PORT)
+    connectDB()
 })
 
-const jj = ff;
