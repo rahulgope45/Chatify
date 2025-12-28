14c24bbb67e58821c1f24e34eca7981827dbe37b
rahulgope45 <rahuleile456@gmail.com>
2025-12-28 20:47:49 +0530

    Chat display fixed with formated  time

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,21 +1,23 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import ChatHeader from './ChatHeader';
 import MessageInput from './MessageInput';
 import MessageSkeleton from './skeletons/MessageSkeleton';
+import { useAuthStore } from '../store/useAuthStore';
+import { formatMessageTime } from '../lib/utils';
 
 
 
 function ChatContainer() {
-    const {messages, getMessages, isMessagesLoading, selectedUser} = useChatStore();
-    
-    useEffect(() => {
-       getMessages(selectedUser._id) 
-    },[selectedUser._id,getMessages])
+  const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
+  const { authUser } = useAuthStore()
+  useEffect(() => {
+    getMessages(selectedUser._id)
+  }, [selectedUser._id, getMessages])
 
 
 
-     if (isMessagesLoading) {
+  if (isMessagesLoading) {
     return (
       <div className="flex-1 flex flex-col overflow-auto">
         <ChatHeader />
@@ -24,12 +26,47 @@ function ChatContainer() {
       </div>
     );
   }
-    
+
   return (
     <div className="flex-1 flex flex-col overflow-auto">
-        <ChatHeader/>
-        <p>Messages..</p>
-        <MessageInput/>
+      <ChatHeader />
+      <div className='flex-1 overflow-y-auto p-4 space-y-4'>
+        {messages.map((messages) => (
+          <div key={messages._id}
+            className={`chat ${messages.senderId === authUser._id ? "chat-end" : "chat-start"}  `}
+          >
+            <div className='chat-image avatar'>
+              <div className='size-5 rounded-full border '>
+                <img
+                  src={
+                    messages.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUser.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(messages.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {messages.image && (
+                <img
+                  src={messages.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {messages.text && <p>{messages.text}</p>}
+            </div>
+          </div>
+        )
+        )}
+      </div>
+      <MessageInput />
     </div>
   )
 }
