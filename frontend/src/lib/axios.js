2c938e2a84b43af61932f267c7f5ff750721eb19
rahulgope45 <rahuleile456@gmail.com>
2026-01-03 03:32:48 +0530

    cross domain added 2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const axiosInstances = axios.create({
-    baseURL: import.meta.env.VITE_API_URL + "/api",
-    withCredentials: true
-})
+  baseURL: import.meta.env.VITE_API_URL + "/api",
+  withCredentials: true,
+});
