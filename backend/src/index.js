cb8d869e4331a6ed6cf6ed639e589790f7fad9bc
rahulgope45 <rahuleile456@gmail.com>
2026-01-03 02:53:07 +0530

    production env setup 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(frontendDistPath));
 
     // Catch-all route for React Router
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(frontendDistPath, "index.html"));
     });
 }
